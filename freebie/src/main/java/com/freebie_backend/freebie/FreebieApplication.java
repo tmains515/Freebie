e33821dd4724492a8fa21b11831a16af78215531
Tyler <t.mains515@hotmail.com>
2025-04-03 22:22:12 -0700

    initial

---
+++ freebie/src/main/java/com/freebie_backend/freebie/FreebieApplication.java
@@ -0,0 +1,13 @@
+package com.freebie_backend.freebie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreebieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreebieApplication.class, args);
+	}
+
+}
---
+++ freebie/src/test/java/com/freebie_backend/freebie/FreebieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freebie_backend.freebie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
